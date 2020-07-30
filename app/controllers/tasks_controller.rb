ed719ad4ec36662c71cce7a1d4727fe74cd1e651
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-07-30 23:53:26 +0900

    setup Task: Controller, Model and View pages.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,43 @@
 class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
+    @task = Task.new
   end
+
+  def show
+  end
+
+  def create
+    @task = Task.create(task_params)
+    @tasks = Task.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:title, :user_id)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])
+    end
+
 end
