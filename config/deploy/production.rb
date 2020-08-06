0a796c96b8c3709a9555fb8a3846f3b2279ea8e1
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-06 17:12:28 +0900

    Modified AWS deploy setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,9 @@
-server '54.150.219.103', user: 'ryo', roles: %w{app db web}
-
-set :ssh_options, {
+server '54.150.219.103',
+user: "ryo",
+roles: %w{app db web},
+ssh_options: {
   port: 22,
+  user: "ryo",
   forward_agent: true,
   keys: ['~/.ssh/myapp_key_rsa']
 }
