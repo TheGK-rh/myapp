e9aabb87d60db5712e1145d8b4ae0015392529eb
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-06 16:23:02 +0900

    modified deploy setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
 server '54.150.219.103', user: 'ryo', roles: %w{app db web}
 
-set :ssh_options, keys: '~/.ssh/myapp_key_rsa'
+set :ssh_options, {
+  port: 22,
+  forward_agent: true,
+  keys: ['~/.ssh/myapp_key_rsa']
+}
