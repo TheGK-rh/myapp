578d395b083ef3f601bae75fe177d8d72a2e5d20
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-06 20:26:29 +0900

    Modified deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '54.150.219.103', user: 'ryo', roles: %w[app db web]
 set :ssh_options, {
   forward_agent: true,
-  user: fetch(:user),
-  keys: %w(~/.ssh/my-key.pem)
+  user: 'ryo',
+  keys: '~/.ssh/my-key.pem'
 }
