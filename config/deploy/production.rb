5b1a56440c4a2616e2c550deadf25b552dd173fe
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-07 23:10:22 +0900

    Modified deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,9 @@
-server '54.150.219.103', user: 'ryo', roles: %w[app db web]
-set :ssh_options, keys: '~/.ssh/myapp_key_rsa'
+server '54.150.219.103',
+ user: 'ryo',
+ roles: %w[app db web],
+ ssh_options: {
+   port: 22,
+   user: "ryo",
+   keys: %w('~/.ssh/myapp_key_rsa'),
+   forward_agent: true
+ }
