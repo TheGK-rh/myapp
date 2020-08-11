69884575af8aae98fd20016c119e54e4070db9e8
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-11 16:54:23 +0900

    There is a problem caused by gem:fog-aws, so fix some

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,16 +7,13 @@ CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
-    config.fog_provider = 'fog-aws'
-    config.fog_directory = 'lemonfish'
-    #IAM_profile
     config.fog_credentials = {
       provider: 'AWS',
-      #環境変数で管理
       aws_access_key_id: ["#{ENV['AWS_ACCESS_KEY_ID']}"],
       aws_secret_access_key: ["#{ENV['AWS_SECRET_ACCESS_KEY']}"],
+      use_iam_profile: true,
       region: 'ap-northeast-1' #東京
     }
+    config.fog_directory = 'lemonfish'
   end
 end
