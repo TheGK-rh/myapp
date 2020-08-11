a6141642655c97f43f80d49f1307e344290f160b
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-11 09:50:31 +0900

    Setup to auto testing by RSpec

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :task
   belongs_to :user
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 120 }
 
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe Comment, type: :model do
 
   let(:comment) { create(:comment) }
 
-  it 'タスク詳細作成が可能である' do
+  it 'コメント作成が可能である' do
     expect(comment).to be_valid
   end
+
+  it 'コメントが120文字以上は登録できない' do
+    expect(build(:comment, body: "a" * 120)).to_not be_valid
+  end
+
+
 
 end
