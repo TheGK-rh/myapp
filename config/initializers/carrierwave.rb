db14d3ce9a6f2e3a13e9f19f2c73fed43a8df537
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-11 13:40:29 +0900

    Apply S3/AWS for the app

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,23 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+#画像名に日本語が使用可能
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+if Rails.env.production?
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory = 'lemonfish'
+  #IAM_profile
+  config.fog_credentials = {
+    provider: 'AWS',
+    #環境変数で管理
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    region: 'ap-northeast-1' #東京
+  }
+else
+  config.storage :file
+  config.enable_processing = false if Rails.env.test? #test :処理スキップ
+end
