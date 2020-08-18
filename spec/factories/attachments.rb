a4d97b3ba7903a1d75b99df4c8f1cb1e69ea6d61
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-18 14:00:03 +0900

    setup for rspec for model: attachment

--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :attachment do
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/rspec_test.jpg')) }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/rspec_test.jpeg')) }
     created_at { Time.now }
     updated_at { Time.now }
     user_id { 1 }
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Attachment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:attachment) { create(:attachment) }
+
+  describe '有効性' do
+
+    it '画像アップロードが可能である' do
+      expect(attachment).to be_valid
+    end
+
+    context 'ファイルの添付なし' do
+      it '登録できない' do
+        attachment.image = nil
+        expect(attachment).to_not be_valid
+      end
+    end
+
+    context 'task_idがnilの場合' do
+      it '作成できない' do
+        attachment.task_id = nil
+        expect(attachment).to_not be_valid
+      end
+    end
+
+    context 'user_idがnilの場合' do
+      it '作成できない' do
+        attachment.user_id = nil
+        expect(attachment).to_not be_valid
+      end
+    end
+
+  end
+
 end
