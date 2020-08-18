96bdcac3a68952c81fed5c0e861fbf52af2f2b07
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-18 14:43:27 +0900

    setup single RSpec test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
 
-  describe "GET /users/sign_in" do
-    it 'ログイン画面の表示に成功すること' do
-      get new_user_session_path
-      expect(response).to have_http_status(200)
-    end
-  end
-
 end
