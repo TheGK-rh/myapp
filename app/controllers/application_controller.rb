d8befc68eae5dad345e7b3700be76d49f3cf70fa
TheGK-rh <hashiguchi.rh.0909@gmail.com>
2020-08-27 00:31:29 +0900

    Completed i18n support of the initialization to apply internationalization on the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ロケール振り分けを全てのアクションで実行
+  around_action :switch_locale
+
+  # params値のロケールによる振り分け
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  # url_for関係メソッドでロケールを設定するよう上書き
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   private
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,48 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|ja/ do
 
-  #ユーザー機能：Devise(including Follow function)
-  devise_for :users, controllers:{
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    passwords: 'users/passwords'
-  }
-  devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-    get 'profile/:id/' => 'users/registrations#profile', as: 'profile'
-    get 'profile/:id/following', to: 'users/registrations#following', as: 'following'
-    get 'profile/:id/followers', to: 'users/registrations#followers', as: 'followers'
-  end
-  #フォロー機能
-  resources :relationships, only: [:create, :destroy]
-  #タグ機能
-  resources :tags, except: :show
-  #アクティビティ・通知機能
-  resources :notifications, only: :index
-  resources :activities, only: :index
-  #基本機能（タスク関連）
-  resources :tasks do
-    member do
-      patch :gotowork
-      patch :gotocomplete
-      patch :backtodolist
+    #ユーザー機能：Devise(including Follow function)
+    devise_for :users, controllers:{
+      registrations: 'users/registrations',
+      sessions: 'users/sessions',
+      passwords: 'users/passwords'
+    }
+    devise_scope :user do
+      post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+      get 'profile/:id/' => 'users/registrations#profile', as: 'profile'
+      get 'profile/:id/following', to: 'users/registrations#following', as: 'following'
+      get 'profile/:id/followers', to: 'users/registrations#followers', as: 'followers'
     end
-    resources :descriptions, only: [:create, :edit, :update, :destroy]
-    resources :comments, only: [:create, :destroy]
-    resources :attachments, only: [:create, :destroy] do
+    #フォロー機能
+    resources :relationships, only: [:create, :destroy]
+    #タグ機能
+    resources :tags, except: :show
+    #アクティビティ・通知機能
+    resources :notifications, only: :index
+    resources :activities, only: :index
+    #基本機能（タスク関連）
+    resources :tasks do
       member do
-        get :download
+        patch :gotowork
+        patch :gotocomplete
+        patch :backtodolist
+      end
+      resources :descriptions, only: [:create, :edit, :update, :destroy]
+      resources :comments, only: [:create, :destroy]
+      resources :attachments, only: [:create, :destroy] do
+        member do
+          get :download
+        end
+      end
+      resources :subtasks, only: [:create, :destroy] do
+        member do
+          patch :complete
+          patch :purge
+        end
       end
     end
-    resources :subtasks, only: [:create, :destroy] do
-      member do
-        patch :complete
-        patch :purge
-      end
-    end
+    get '/:locale' => 'static_pages#home'
+    #Home page
+    root "tasks#index"
   end
-
-  #welcome page(on working)
-  get 'homes/index'
-  #Home page
-  root "tasks#index"
-
 end
